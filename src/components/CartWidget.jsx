e7052305dd55fdd8170e46f85729d8715d09a26c
Maria Pia Zaniboni <mpiazaniboni@gmail.com>
2024-12-12 15:28:21 -0300

    ultimo commit Primera Pre Entrega

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,7 +1,5 @@
 import { FaShoppingCart } from "react-icons/fa";
 
-//Icono de carrito con una notificacion mostrando nuemero harcodeado (fijo)
-
 export default function CartWidget (props) {
 
     const numProducts = 1;// cuando cambie deberia ser let
