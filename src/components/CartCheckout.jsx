e1086e361c8e0fadcc632c0d84b391bdc799b0f9
Maria Pia Zaniboni <mpiazaniboni@gmail.com>
2025-02-07 19:09:48 -0300

    feat(form)

--- src/components/CartCheckout.jsx
+++ src/components/CartCheckout.jsx
@@ -9,15 +9,33 @@ export default function CartCheckout (props) {
     const { cartItems, getTotalPrice, clearCart } = useContext(cartContext);
     const navigate = useNavigate();
 
-    async function handleCheckout () {
+    const styleInput = "border border-gray-300 p-2 rounded-md flex-1 bg-white";
+
+    const [userData, setUserData] = useState({
+        username: "",
+        surname: "",
+        age: "",
+      });
+    
+    function onInputChange(evt) {
+        const inputName = evt.target.name;
+        const newUserData = { ...userData };
+        newUserData[inputName] = evt.target.value;
+        setUserData(newUserData);
+    }
+
+    async function handleCheckout (evt) {
+        evt.preventDefault();
+
+        console.log("hola");
         setErrorMessage(null);
         setSuccessMessage(null);
 
         const orderData = {
             buyer: {
-                name: "bruno",
-                email: "mail"
-
+                username: userData.username,
+                surname: userData.surname,
+                age: userData.age,
             },
             cartItems,
             total: getTotalPrice(),
@@ -40,12 +58,46 @@ export default function CartCheckout (props) {
          });
     }
 
-
     return (
-        <div>
-            <button className="rounded-[5px] bg-slate-950 px-4 py-2 text-white ml-4 mt-14" onClick={handleCheckout}>Finalizar compra</button> 
-            {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-            {successMessage && <p style={{ color: "green" }}>{successMessage}</p>}
+        <form>
+        <div className="flex mb-2">
+            <label className="w-[100px] mr-2">Nombre</label>
+            <input
+            name="username"
+            type="text"
+            onChange={onInputChange}
+            className={styleInput}
+            />
         </div>
+
+        <div className="flex mb-2">
+            <label className="w-[100px] mr-2">Apellido</label>
+            <input
+            name="surname"
+            type="text"
+            onChange={onInputChange}
+            className={styleInput}
+            />
+        </div>
+
+        <div className="flex mb-2">
+            <label className="w-[100px] mr-2">Edad</label>
+            <input
+            name="age"
+            type="text"
+            onChange={onInputChange}
+            className={styleInput}
+            />
+        </div>
+
+        <button className="rounded-[5px] bg-slate-950 px-4 py-2 text-white ml-4 mt-14" 
+            disabled={!( userData.username !== "" && userData.surname !== "" && userData.age !== "")}
+            onClick={handleCheckout}
+        >
+            Finalizar compra
+        </button>
+        {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+        {successMessage && <p style={{ color: "green" }}>{successMessage}</p>}
+    </form>
     );
 } 
